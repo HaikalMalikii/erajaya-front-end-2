ac3d5fd9b60db7c47561c5411fa6dc57bb09cd20
HaikalMalikii <muhamad.maliki@binus.ac.id>
2022-12-14 11:00:41 +0700

    Login form

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './component/Login';
 import data from './data.json';
+import Navbar from './component/Navbar';
 
 
 
 function App() {
-
-
   return (
     <div className="App">
-      <Login email="Email default test@gmail.com" password="Pass Default 123abc" />
-      {
-        data.map(datas => {
-          return (
-            <div className='box' key={datas.id}>
-              {datas.nama}
-              {datas.job}
-            </div>
-          )
-        })
-      }
+      <div className='Header'>
+        <Navbar />
+      </div>
+      <div className='Body'>
+        <Login email="Email default test@gmail.com" password="Pass Default 123abc" />
+        <div className='content'>
+          {
+            data.map(datas => {
+              return (
+                <div className='box' key={datas.id}>
+                  {datas.nama}
+                  {datas.job}
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
